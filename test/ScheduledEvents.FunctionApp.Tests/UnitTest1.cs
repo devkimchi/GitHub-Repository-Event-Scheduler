a6915932c2f15bef53c525fc088f4c918a9ddbe1
Justin Yoo <justin.yoojh@gmail.com>
2020-03-16 17:32:02 +0900

    Initial code commit

---
+++ test/ScheduledEvents.FunctionApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScheduledEvents.FunctionApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
